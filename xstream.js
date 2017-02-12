fb0b07cbff67b9a0b7ead73799face6d729bcba8
Ashley Cutmore <ashley.cutmore@gmail.com>
2017-02-12 18:56:46 +0000

    NEW:  xstream single value stacks

---
+++ xstream.js
@@ -0,0 +1,41 @@
+const xs = require('xstream').default;
+const stackTrace = require('./stacktrace');
+const log = require('./log');
+
+function main() {
+    'use strict';
+
+    xs.of(1)
+        .subscribe({
+            next() {
+                log('xs.of(1)');
+                log(stackTrace());
+                [
+                    "Object.next",
+                    "Stream._n",
+                    "FromArray._start",
+                    "Stream._add",
+                    "Stream.addListener",
+                    "Stream.subscribe",
+                    "main"
+                ]
+            }
+        });
+
+    let listener = undefined;
+    xs.create({ start: l => listener = l, stop: () => void 0 })
+        .subscribe({
+            next() {
+                log('xs.create(); listener.next()');
+                log(stackTrace());
+                [
+                    "Object.next",
+                    "Stream._n",
+                    "main"
+                ]
+            }
+        });
+    listener.next();
+};
+
+main();
