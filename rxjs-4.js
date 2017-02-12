9dd46faf19480be79fcc8520d1651e28bf14b0b1
Ashley Cutmore <ashley.cutmore@gmail.com>
2017-02-12 13:44:45 +0000

    NEW: basic rxjs-4 onNext stacks logged

---
+++ rxjs-4.js
@@ -0,0 +1,53 @@
+const Rx = require('Rx');
+const stackTrace = require('./stacktrace');
+const log = require('./log');
+
+function main() {
+    'use strict';
+
+    Rx.Observable.just(1)
+        .subscribe(v => {
+            log('Rx.Observable.just(1)');
+            log(stackTrace());
+            [
+                "AnonymousObserver._onNext",
+                "AnonymousObserver.Rx.AnonymousObserver.AnonymousObserver.next",
+                "AnonymousObserver.Rx.internals.AbstractObserver.AbstractObserver.onNext",
+                "AnonymousObserver.tryCatcher",
+                "AutoDetachObserverPrototype.next",
+                "AutoDetachObserver.Rx.internals.AbstractObserver.AbstractObserver.onNext",
+                "scheduleItem",
+                "JustObservable.subscribeCore",
+                "JustObservable.tryCatcher",
+                "setDisposable",
+                "ScheduledItem.invokeCore",
+                "ScheduledItem.invoke",
+                "runTrampoline",
+                "tryCatcher",
+                "CurrentThreadScheduler.schedule",
+                "JustObservable.Rx.ObservableBase.ObservableBase._subscribe",
+                "JustObservable.Rx.Observable.observableProto.subscribe.observableProto.forEach",
+                "main"
+            ]
+        });
+
+    let observer = null;
+    Rx.Observable.create(o => observer = o)
+        .subscribe(v => {
+            log('Rx.Observable.create(); observer.onNext()');
+            log(stackTrace());
+            [
+                "AnonymousObserver._onNext",
+                "AnonymousObserver.Rx.AnonymousObserver.AnonymousObserver.next",
+                "AnonymousObserver.Rx.internals.AbstractObserver.AbstractObserver.onNext",
+                "AnonymousObserver.tryCatcher",
+                "AutoDetachObserverPrototype.next",
+                "AutoDetachObserver.Rx.internals.AbstractObserver.AbstractObserver.onNext",
+                "main"
+            ]
+        });
+
+    observer.onNext();
+};
+
+main();
